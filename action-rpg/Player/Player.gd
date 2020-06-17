26bdc15d79956bc808b08f88f732aac10af795e4
Filip Lindahl <filip.g.lindahl@gmail.com>
2020-06-17 21:26:30 +0200

    animation+tree, smooth. A.tree much less code. very cool

--- action-rpg/Player/Player.gd
+++ action-rpg/Player/Player.gd
@@ -6,6 +6,12 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+onready var animation_player = $AnimationPlayer
+onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
+
+
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,8 +19,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if(input_vector != Vector2.ZERO):
+		animation_tree.set("parameters/idle/blend_position", input_vector)
+		animation_tree.set("parameters/run/blend_position", input_vector)
+		animation_state.travel("run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animation_state.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
