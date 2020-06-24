2ca4afe3c627d227b9861f0033e18232281434e1
Filip Lindahl <filip.g.lindahl@gmail.com>
2020-06-24 21:27:13 +0200

    player health UI, shadows, and also trees

--- action-rpg/Stats.gd
+++ action-rpg/Stats.gd
@@ -1,11 +1,22 @@
 extends Node
 
-export (int) var MAX_HEALTH = 1
-onready var health = MAX_HEALTH setget set_health
+export (int) var max_health = 1 setget set_max_health
+var health = max_health setget set_health
 
 signal no_health
+signal health_changed(new_health)
+signal max_health_changed(new_max_health)
 
 func set_health(new_health):
 	health = new_health
+	emit_signal("health_changed", new_health)
 	if health <= 0:
 		emit_signal("no_health")
+
+func set_max_health(new_max_health):
+	max_health = new_max_health
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
+
+func _ready():
+	self.health = max_health
