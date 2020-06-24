a04ff8748de8e577626ff1238804cfa224e6224b
Filip Lindahl <filip.g.lindahl@gmail.com>
2020-06-24 08:13:40 +0200

    simple bat AI and state, and body-detection

--- action-rpg/Enemies/Bat.gd
+++ action-rpg/Enemies/Bat.gd
@@ -2,14 +2,45 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+
+enum {IDLE, WANDER, CHASE}
+
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
+var state = CHASE
+
 onready var stats = $Stats
+onready var player_detection = $PlayerDetection
+onready var sprite = $AnimatedSprite
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = player_detection.player
+			if player != null:
+				var target_position = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(target_position * MAX_SPEED, ACCELERATION * delta)
+				sprite.flip_h = velocity.x < 0
+			else:
+				state = IDLE
+	velocity = move_and_slide(velocity)
 
+func seek_player():
+	if player_detection.can_see_player():
+		state = CHASE
+	
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
---
+++ action-rpg/Enemies/PlayerDetection.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var player = null
+
+
+func can_see_player():
+	return player != null
+
+func _on_PlayerDetection_body_entered(body):
+	player = body
+
+
+func _on_PlayerDetection_body_exited(body):
+	player = null
